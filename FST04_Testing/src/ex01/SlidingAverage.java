1605d42c33f370926a2f74ebb653b5ea19ace0c3
dummy <dummy@github.com>
2020-04-25 13:57:40 +0200

    Added IssueList.txt template, method specification and reference results Improved documentation on TestCases
    Migrated from JUnit 5 to JUnit 4 as JUnit 5 is not available in the VM.

--- FST04_Testing/src/ex01/SlidingAverage.java
+++ FST04_Testing/src/ex01/SlidingAverage.java
@@ -3,10 +3,17 @@ package ex01;
 public class SlidingAverage {
 	
 	/* Average of neighboring elements in array
-	 * Author: Danilo Beuche / Last Change: 21.4.2020 / Version: 1.2
+	 * Author: Danielo Beuche
+	 * 
+	 * Sliding average of an value is calculated by averaging two right and left values and the value itself
+	 * If there are not enough neighbors available, the value itself is place in the output array.
+	 * The output array may be shorter than the defined length of the to be processed input values
+	 * In this case the output array contains the appropriate values and the method returns normally  
+	 * 
 	 */ 
 	 
-	public static void average(double[] in, double[] out, int length) { /* loop over all elements in array */
+	public static void average(double[] in, double[] out, int length) { 
+		/* loop over all elements in 'in' array */
 		for (int i = 0; i < length; i++) {
 			// Compare index with 1 or length -1, only do average in this case
 			if (i > 1 || i < length - 1) {
@@ -22,6 +29,8 @@ public class SlidingAverage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			double in[]= {10.0 , 0.2 , 4.4, 5.6, 7.3, 8.5 };
+			// sliding average output for this input should be 
+			// { 10.0, 0.2, 5.5, 5.2, 7.3, 8.5 }
 			
 			double out[] = new double[5];
 			
--- FST04_Testing/src/ex01/TestCases.java
+++ FST04_Testing/src/ex01/TestCases.java
@@ -3,53 +3,65 @@
  */
 package ex01;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 /**
  * @author danilo
  *
  */
-class TestCases {
+public class TestCases {
 
 	// the two tests below should help you to find your way around, even if you are not too familiar with Java and JUnit 
 	@Test
-	void testExample01_Passing() {
+	public void testExample01_Passing() {
+		// compare result for array length 1 with expected result 
 		double in[]= {10.0 , 0.2 , 4.4, 5.6, 7.3, 8.5 };
 		
-		double out[] = new double[5];
+		double out[] = new double[1];
+		double reference[] = { 10.0 };// the expected sliding average value(s)
 		
 		
 		SlidingAverage.average(in, out, 1);
 		
-		// you can compare 2 scalar values
-		assertEquals(10.0, out[0]);
-		
+		for (int idx = 0; idx < 1; idx++)
+		{
+			// you can compare 2 scalar values
+			assertEquals(in[idx], out[idx],0.0); 
+			// all these assertXXXX statements in a test must yield true to get a successfully passed test.
+			// 
+			// the third argument 0.0 is how much both values may differ from each other to be still considered equal
+			// this is because sometimes rounding error create almost identical results where the difference is not relevant
+			// we go for exact match thus 0.0
+		}
 		// but you can compare also arrays
-		assertArrayEquals(in , in);
+		assertArrayEquals(reference , out, 0.0);
 		
 		// you can type assert and then CTRL+SPACE aka STRG+SPACE to see more choices of available assert functions
-		// but those 2 plus possible assertThrows should get the job done for you
+		// but those 2 (plus possibly assertThrows) should get the job done for you
 	}
 	
 	@Test
-	void testExample02_Failing() {
+	public void testExample02_Failing() {
+		// compare result for array length 1 with expected result
+		// this test is the same as above, but this time failing since
+		// the expected reference result is not identical to the delivered result
+		// however, the fault this time is in the test case, not the tested method
 		double in[]= {10.0 , 0.2 , 4.4, 5.6, 7.3, 8.5 };
 		
-		double out[] = new double[5];
+		double out[] = new double[1];
+		double reference[] = { 100.0 }; // the expected sliding average value(s)
 		
 		
 		SlidingAverage.average(in, out, 1);
 		
-		// you can compare 2 scalar values, this will fail since 1.0 != 10.0
-		assertEquals(1.0, out[0]);
-		
-		// but you can compare also arrays, this will fail, too, since in does not have same content as out
-		assertArrayEquals(in , out);
-		
-		// you can type assert and then CTRL+SPACE aka STRG+SPACE to see more choices of available assert functions
-		// but those 2 plus possible assertThrows should get the job done for you
+		for (int idx = 0; idx < 1; idx++)
+		{
+			// you can compare 2 scalar values
+			assertEquals(in[idx], out[idx], 0.0);
+		}
+		// but you can compare also arrays
+		assertArrayEquals(reference , out, 0.0);		
 	}
-
 }
